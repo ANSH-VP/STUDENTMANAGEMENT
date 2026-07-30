98edcce7d38d3fa599e7d481acb3f032bbf743dd
unknown <2500032124@kluniversity.in>
2026-07-30 15:08:46 +0530

    Added HTML files

--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("ANSH IS A LEGEND");
-    return 0;
-}
--- test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello from Pull Request!");
-    return 0;
-}

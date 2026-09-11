88e7b297a02af375e9000badaabaa66a17b1ad4c
ANSH-VP <2500032124@kluniversity.in>
2026-09-11 09:46:09 +0530

    Changes have done

--- script1.js
+++ script1.js
@@ -2,3 +2,7 @@ function fun(){
         document.getElementById('demo').innerHTML = 'Hello, ANSH!'
         console.log("ansh is a fucking legend")
 }
+function ansh(){
+        console.log("ANSH IS A  LEGEND ")
+}
+ansh()
